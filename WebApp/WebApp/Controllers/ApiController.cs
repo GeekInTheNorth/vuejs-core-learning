352e195cc4f6fe7329bbe756d4f6b44cbe04903c
Mark Stott <mypantsareontheoutside@gmail.com>
2018-01-24 13:59:40 +0000

    Implement binding data from API

---
+++ WebApp/WebApp/Controllers/ApiController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    public class DataController : Controller
+    {
+        [HttpGet]
+        public IEnumerable<string> GetList()
+        {
+            return new List<string>
+            {
+                "One",
+                "Two",
+                "Three",
+                "Many!"
+            };
+        }
+    }
+}
---
+++ WebApp/WebApp/wwwroot/js/Home/LoadFromApi.js
@@ -0,0 +1,12 @@
+﻿var app = new Vue({
+    el: '#app',
+    data: {
+        results: []
+    },
+    mounted() {
+        var url = window.location.origin + "/api/Data";
+        $.get(url).done(data => {
+            this.results = data;
+        });
+    }
+});
